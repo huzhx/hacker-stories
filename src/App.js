c81cb1e660e2abd972bc0ec786b5329b6193ba07
Zhaoxian Hu <huzhx@uci.edu>
2020-12-14 23:18:40 -0800

    render simple variables in jsx

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import './App.css';
 
-const title = 'React';
+const welcome = {
+  greeting: 'Hey',
+  title: 'React',
+};
+
+function getTitle(title) {
+  return title;
+}
 
 function App() {
   return (
     <div>
-      <h1>Hello {title}</h1>
+      <h1>
+        {welcome.greeting} {getTitle(welcome.title)}
+      </h1>
+      <label htmlFor="">Search:</label>
+      <input id="search" type="text" />
     </div>
   );
 }
