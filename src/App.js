c757175e24a43258b4ecb57196041215a60b2375
Zhaoxian Hu <huzhx@uci.edu>
2020-12-17 23:57:58 -0800

    cleaned unused variable

--- src/App.js
+++ src/App.js
@@ -5,25 +5,6 @@ const welcome = {
   title: 'React',
 };
 
-const list = [
-  {
-    title: 'React',
-    url: 'https://reactjs.org/',
-    author: 'Jordan Walke',
-    num_comments: 3,
-    points: 4,
-    objectID: 0,
-  },
-  {
-    title: 'Redux',
-    url: 'https://redux.js.org/',
-    author: 'Dan Abramov, Andrew Clark',
-    num_comments: 2,
-    points: 5,
-    objectID: 1,
-  },
-];
-
 function getTitle(title) {
   return title;
 }
