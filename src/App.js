a51d5798610057834dbaeec81bac71df31ea246f
Zhaoxian Hu <huzhx@uci.edu>
2020-12-17 23:57:04 -0800

    learned how to transmit data from one component to another

--- src/App.js
+++ src/App.js
@@ -29,6 +29,25 @@ function getTitle(title) {
 }
 
 function App() {
+  const stories = [
+    {
+      title: 'React',
+      url: 'https://reactjs.org/',
+      author: 'Jordan Walke',
+      num_comments: 3,
+      points: 4,
+      objectID: 0,
+    },
+    {
+      title: 'Redux',
+      url: 'https://redux.js.org/',
+      author: 'Dan Abramov, Andrew Clark',
+      num_comments: 2,
+      points: 5,
+      objectID: 1,
+    },
+  ];
+
   const handleChange = (event) => {
     console.log(event.target.value);
   };
@@ -41,13 +60,13 @@ function App() {
       <label htmlFor="">Search:</label>
       <input id="search" type="text" onChange={handleChange} />
       <hr />
-      <List />
+      <List list={stories} />
     </div>
   );
 }
 
-function List() {
-  return list.map((item) => {
+const List = (props) =>
+  props.list.map((item) => {
     return (
       <div key={item.objectID}>
         <span>
@@ -59,6 +78,5 @@ function List() {
       </div>
     );
   });
-}
 
 export default App;
