95feaea845ef9d6af064f3eabe7c15cb7af8c560
Zhaoxian Hu <huzhx@uci.edu>
2020-12-17 23:16:31 -0800

    refactor and put list into its own component

--- src/App.js
+++ src/App.js
@@ -36,20 +36,25 @@ function App() {
       </h1>
       <label htmlFor="">Search:</label>
       <input id="search" type="text" />
-      {list.map((item) => {
-        return (
-          <div key={item.objectID}>
-            <span>
-              <a href={item.url}>{item.title}</a>
-            </span>
-            <span>{item.author}</span>
-            <span>{item.num_comments}</span>
-            <span>{item.points}</span>
-          </div>
-        );
-      })}
+      <hr />
+      <List />
     </div>
   );
+}
+
+function List() {
+  return list.map((item) => {
+    return (
+      <div key={item.objectID}>
+        <span>
+          <a href={item.url}>{item.title}</a>
+        </span>
+        <span>{item.author}</span>
+        <span>{item.num_comments}</span>
+        <span>{item.points}</span>
+      </div>
+    );
+  });
 }
 
 export default App;
