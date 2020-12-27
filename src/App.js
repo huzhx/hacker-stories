42da3c6f7eab457c3dbe3f67d52ba49b2beda511
Zhaoxian Hu <huzhx@uci.edu>
2020-12-26 20:19:47 -0800

    learned how to set up the project folder structure

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import axios from 'axios';
 import './App.css';
+import List from './List';
+import SearchForm from './SearchForm';
 
 const welcome = {
   greeting: 'Hey',
@@ -105,57 +107,5 @@ function App() {
     </div>
   );
 }
-
-const SearchForm = ({ searchTerm, onSearchInput, onSearchSubmit }) => {
-  return (
-    <form onSubmit={onSearchSubmit}>
-      <InputWithLabel id="search" value={searchTerm} onInputChange={onSearchInput} isFocused>
-        <strong>Search:</strong>
-      </InputWithLabel>
-      <button type="submit" disabled={!searchTerm}>
-        Submit
-      </button>
-    </form>
-  );
-};
-
-const InputWithLabel = ({ id, value, type = 'text', onInputChange, isFocused, children }) => {
-  const inputRef = React.useRef();
-
-  React.useEffect(() => {
-    if (isFocused && inputRef.current) {
-      inputRef.current.focus();
-    }
-  }, [isFocused]);
-
-  return (
-    <>
-      <label htmlFor={id}>{children}</label>
-      &nbsp;
-      <input ref={inputRef} id={id} type={type} value={value} onChange={onInputChange} />
-    </>
-  );
-};
-
-const List = ({ list, onRemoveItem }) =>
-  list.map((item) => <Item key={item.objectID} item={item} onRemoveItem={onRemoveItem} />);
-
-const Item = ({ item, onRemoveItem }) => {
-  return (
-    <div>
-      <span>
-        <a href={item.url}>{item.title}</a>
-      </span>
-      <span>{item.author}</span>
-      <span>{item.num_comments}</span>
-      <span>{item.points}</span>
-      <span>
-        <button type="button" onClick={() => onRemoveItem(item)}>
-          Dismiss
-        </button>
-      </span>
-    </div>
-  );
-};
 
 export default App;
